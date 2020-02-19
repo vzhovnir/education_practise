1a8541215366c4e9c8200e00724e1d673a8ac99e
Vladislav <vzhovnir@griddynamics.com>
2020-02-19 12:29:24 +0000

    modified code if changed on  elif

--- Task2.py
+++ Task2.py
@@ -4,8 +4,8 @@ for x in range(1,100):
     s=''
     if x % 3==0:
         s+="Fizz"
-    if x % 5==0:
+    elif x % 5==0:
         s+="Buzz"
-    if s=='':
+    elif s=='':
         s=x
     print(s,end=' ') 
