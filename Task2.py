df5c8515924aea5ac4d511a2972220ab11df3993
Vladislav <vzhovnir@griddynamics.com>
2020-02-19 12:01:57 +0000

    Task2 Python BizzBuzz

---
+++ Task2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+for x in range(1,100):
+    s=''
+    if x % 3==0:
+        s+="Fizz"
+    if x % 5==0:
+        s+="Buzz"
+    if s=='':
+        s=x
+    print(s,end=' ') 
