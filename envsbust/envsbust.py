1fbd2803913b148c7bc62486fdb9a8d881d56e10
Vladislav <vzhovnir@griddynamics.com>
2020-02-21 15:30:56 +0000

    envsbust Python version

---
+++ envsbust/envsbust.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys 
+from subprocess import run
+
+
+tmp = {'Mobile':'+38099999999',
+'Office': 'Grid Dynamics',
+ 'Position':'Intern'}
+
+
+def main():
+    read_line=[]
+    file_name = sys.argv[1]
+    new_file_name = "upt_" + file_name
+    k=[]
+    with open(file_name, 'r') as f :
+        for i in f :
+            read_line+=[i.format(**tmp)]
+            k=[read_line.replace('$','') for read_line in k]
+            read_line=k
+    with open(new_file_name,"w") as f:
+            for i in read_line:
+                f.write(i+'\n')
+                print(i)
+
+if __name__ == '__main__':
+    print('Successfully')
+    main()
