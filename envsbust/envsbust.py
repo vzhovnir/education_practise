fb43241ea63a6265ff8e81b36fca4df692051570
Vladislav <vzhovnir@griddynamics.com>
2020-02-21 15:50:02 +0000

    rename tmp on dict_paramameters

--- envsbust/envsbust.py
+++ envsbust/envsbust.py
@@ -4,7 +4,7 @@ import sys
 from subprocess import run
 
 
-tmp = {'Mobile':'+38099999999',
+dict_paramameters= {'Mobile':'+38099999999',
 'Office': 'Grid Dynamics',
  'Position':'Intern'}
 
@@ -16,14 +16,13 @@ def main():
     k=[]
     with open(file_name, 'r') as f :
         for i in f :
-            read_line+=[i.format(**tmp)]
+            read_line+=[i.format(**dict_paramameters)]
             k=[read_line.replace('$','') for read_line in k]
             read_line=k
     with open(new_file_name,"w") as f:
             for i in read_line:
                 f.write(i+'\n')
-                print(i)
+                
 
 if __name__ == '__main__':
-    print('Successfully')
-    main()
+     main()
